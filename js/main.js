f6b81f60d6f7f32d615dc6c0511dfd39fd0e436c
inter <dbstjdgur78@naver.com>
2022-11-07 17:54:37 +0900

    commit3

--- js/main.js
+++ js/main.js
@@ -14,3 +14,34 @@ searchInputEl.addEventListener('blur',function(){
   searchEl.classList.remove('focused');
   searchInputEl.setAttribute('placeholder','');
 });
+
+const badgeEl = document.querySelector('header .badges');
+
+window.addEventListener('scroll', _.throttle(function(){
+  console.log(window.scrollY);
+  if(window.scrollY > 500){
+    // 배지 숨기기
+    // gsap.to(요소, 지속시간, 옵션);
+    gsap.to(badgeEl,.6, {
+      opacity: 0,
+      display: 'none'
+
+    });
+  }else{
+    // 배지 보이기
+    gsap.to(badgeEl,.6, {
+      opacity: 1,
+      display: 'block'
+    });
+  }
+},300));
+
+// _.throttle(함수, 시간)
+
+const fadeEls = document.querySelectorAll('.vlsual .fade-in');
+fadeEls.forEach(function(fadeEl, index){
+  gsap.to(fadeEl, 1, {
+    delay: (index+1) * .7, // 0.7, 1.4 , 2.1, 2.8
+    opacity: 1
+  });
+});
