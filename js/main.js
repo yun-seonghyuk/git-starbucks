1d8f0173fb3d27568f0710c0585c2e84795bfdee
inter <dbstjdgur78@naver.com>
2022-11-08 17:51:40 +0900

    3D 애니메이션

--- js/main.js
+++ js/main.js
@@ -84,10 +84,33 @@ let isHidePromotion = false;
 promotionToggleBtn.addEventListener('click',function(){
   isHidePromotion = !isHidePromotion // 반대의값 반환
   if(isHidePromotion){
-    // 숨김 처리!
+    // 숨김 처리
     promotionEl.classList.add('hide');
   }else{
-    // 보임 처리!
+    // 보임 처리
     promotionEl.classList.remove('hide');
   }
 });
+
+// 범위 랜덤 함수(소수점 2자리까지)
+function random(min, max) {
+  // `.toFixed()`를 통해 반환된 문자 데이터를,
+  // `parseFloat()`을 통해 소수점을 가지는 숫자 데이터로 변환
+  return parseFloat((Math.random() * (max - min) + min).toFixed(2))
+}
+
+function floatingObject(selector,delay,size){
+  gsap.to(
+    selector, // 선택자
+    random(1.5, 2.5), // 애니메이션 동작 시간
+    { // 옵션
+    y: size,
+    repeat: -1,
+    yoyo: true,
+    ease: Power1.easeInOut,
+    delay: random(0, delay)
+  });
+}
+floatingObject('.floating1',1,15);
+floatingObject('.floating2',.5,15);
+floatingObject('.floating3',1.5,20);
